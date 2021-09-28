69d0f66f745205a9ebab5d8cda93f9568fb9b390
Ganesh Tiwari <ganesht049@gmail.com>
2021-09-28 13:46:10 +0530

    added contact me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import Lottie from "react-lottie";
 
 import Switch from "../components/Switch";
 import CustomTooltip from "../components/CustomTooltip";
@@ -10,6 +11,7 @@ import ZoominCard from "../components/ZoominCard";
 import { SocialIcon } from "react-social-icons";
 
 import styles from "../styles/Home.module.css";
+import { letterAnimation } from "../animation";
 
 export interface FormState {
   name: string;
@@ -18,6 +20,7 @@ export interface FormState {
 }
 
 const Home: NextPage = () => {
+  const [isLoading, setIsLoading] = useState<boolean>();
   const [showMenu, setShowMenu] = useState<boolean>(false);
 
   const [formState, setFormState] = useState<FormState>({
@@ -25,6 +28,13 @@ const Home: NextPage = () => {
     email: "",
     message: "",
   });
+
+  // default options for the lottie animation
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: letterAnimation,
+  };
 
   return (
     <div className={styles.container}>
@@ -168,24 +178,34 @@ const Home: NextPage = () => {
               <div className="inline-flex flex-row flex-wrap text-2xl mis-8">
                 <div className="tooltip relative">
                   Android, &nbsp;
-                  <CustomTooltip childIcon={"/assets/icons/logos/android-bot.svg"} />
+                  <CustomTooltip
+                    childIcon={"/assets/icons/logos/android-bot.svg"}
+                  />
                 </div>
                 <div className="tooltip relative">
                   Windows, &nbsp;
-                  <CustomTooltip childIcon={"/assets/icons/logos/windows-logo.svg"} />
+                  <CustomTooltip
+                    childIcon={"/assets/icons/logos/windows-logo.svg"}
+                  />
                 </div>
                 <div className="tooltip relative">
                   Linux, &nbsp;
-                  <CustomTooltip childIcon={"/assets/icons/logos/linux-logo.svg"} />
+                  <CustomTooltip
+                    childIcon={"/assets/icons/logos/linux-logo.svg"}
+                  />
                 </div>
                 <div className="tooltip relative">
                   Web &nbsp;
-                  <CustomTooltip childIcon={"/assets/icons/logos/html-logo.svg"} />
+                  <CustomTooltip
+                    childIcon={"/assets/icons/logos/html-logo.svg"}
+                  />
                 </div>
                 and &nbsp;
                 <div className="tooltip relative">
                   Server. &nbsp;
-                  <CustomTooltip childIcon={"/assets/icons/logos/server-farm.svg"} />
+                  <CustomTooltip
+                    childIcon={"/assets/icons/logos/server-farm.svg"}
+                  />
                 </div>
               </div>
             </div>
@@ -221,7 +241,8 @@ const Home: NextPage = () => {
           >
             <div className="text-3xl mli-8 flex flex-col h-full justify-center gap-8">
               <div>
-                I create contents as Blogs, Videos and Different Technical Sessions.
+                I create contents as Blogs, Videos and Different Technical
+                Sessions.
               </div>
               <div className="text-2xl">
                 You can find me on the following Social Networks
@@ -273,6 +294,117 @@ const Home: NextPage = () => {
                     className="w-12 h-12 p-1 border-white border"
                   />
                 </a>
+              </div>
+            </div>
+          </section>
+
+          <section
+            className={`flex md:flex-row flex-col gap-8 justify-center w-full p-8 md:items-center h-screen`}
+          >
+            <div className="flex flex-col h-full w-full justify-center md:flex-row md:px-8">
+              <form
+                name="contact-me"
+                className=" flex flex-col justify-center w-full md:w-1/2 gap-4 p-4"
+                method="POST"
+                onSubmit={async (event) => {
+                  event.preventDefault();
+                  console.log(`submitting`);
+                  setIsLoading(true);
+                  // setTimeout(() => {
+                  //   console.log(`done`);
+                  // },4000)
+                  // await dispatch(queryActions.sendEmail({ data: formState }));
+                  setIsLoading(false);
+                }}
+                data-netlify="true"
+              >
+                <h1 className="text-2xl md:text-4xl font-semibold">
+                  If you have any queries send em here
+                </h1>
+                <input type="hidden" name="form-name" value="contact-me" />
+                <div className="flex flex-col gap-2">
+                  <label
+                    className="text-black dark:text-white"
+                    htmlFor="name-input"
+                  >
+                    Your Name
+                  </label>
+                  <input
+                    name="name-input"
+                    id="name-input"
+                    required
+                    aria-required
+                    value={formState.name}
+                    onChange={(event) => {
+                      setFormState({ ...formState, name: event.target.value });
+                    }}
+                    className="p-2 text-base text-black w-full"
+                    placeholder="Your Name"
+                    type="text"
+                  />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <label className="" htmlFor="query-input">
+                    Your Query
+                  </label>
+                  <textarea
+                    name="query-input"
+                    required
+                    aria-required
+                    id="query-input"
+                    value={formState.message}
+                    onChange={(event) => {
+                      setFormState({
+                        ...formState,
+                        message: event.target.value,
+                      });
+                    }}
+                    className="p-2 text-black text-base w-full"
+                    placeholder="Your Query"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2">
+                  <label
+                    className="text-black dark:text-white"
+                    htmlFor="email-input"
+                  >
+                    Your Email Address
+                  </label>
+                  <input
+                    name="email-input"
+                    required
+                    aria-required
+                    id="email-input"
+                    type="email"
+                    value={formState.email}
+                    onChange={(event) => {
+                      setFormState({ ...formState, email: event.target.value });
+                    }}
+                    placeholder="Your Email Address"
+                    className="p-2 text-black text-base w-full"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className={` dark:text-black border border-black dark:bg-white w-max p-2 pli-4 ${
+                    isLoading ? "disabled:opacity-50" : ""
+                  }`}
+                >
+                  Submit
+                </button>
+              </form>
+
+              <div
+                className={`mlb-8 flex items-center  md:w-1/2 ${styles.hideWhenSmall}`}
+              >
+                <Lottie
+                  speed={0.5}
+                  height="100px"
+                  width="100px"
+                  options={defaultOptions}
+                />
               </div>
             </div>
           </section>
