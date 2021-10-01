f71639053ed7570e22bcb55203715d8248879383
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-01 12:29:56 +0530

    changed plugin load

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer')
-  ]
-}
+  plugins: ["postcss-import", "tailwindcss", "autoprefixer"],
+};
