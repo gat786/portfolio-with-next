1444b3ceb7234c3998917c96d3f7754a2898d6fb
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-02 14:54:40 +0530

    import path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")]
-  }
-}
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
