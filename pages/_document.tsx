7bc61c0ab973cfd953613118c14cbe1ad5843fae
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-02 20:50:35 +0530

    added a custom document to create og-image for website

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta property="og:image" content="/og-image.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
