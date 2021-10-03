e7b4747bc6a4ec0f509bd9aa3202243d7e6b3393
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-03 17:33:19 +0530

    added twitter specific data to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,19 @@ class MyDocument extends Document {
           <meta property="og:type" content="text/html" />
           <meta property="og:url" content="https://gats.dev" />
 
-          <meta name="twitter:card" content="summary"></meta>
+          <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta
+            name="twitter:title"
+            content="Ganesh Tiwari's Portfolio Website"
+          ></meta>
+          <meta
+            name="twitter:description"
+            content="Website listing projects, blogs and content made by Ganesh Tiwari"
+          ></meta>
+          <meta
+            name="twitter:image:src"
+            content="https://res.cloudinary.com/dtldj8hpa/image/upload/c_scale,h_720/v1633188973/gats.dev/og-image.webp"
+          ></meta>
           <meta name="twitter:site" content="@igat786" />
           <meta name="twitter:creator" content="@igat786" />
         </Head>
