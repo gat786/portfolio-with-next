aa4075b489e1bbd02d4bcbe9cc717d297d60f8d6
Ganesh Tiwari <ganesht049@gmail.com>
2022-12-16 23:04:42 +0530

    fix: event cards for dark mode

--- components/events/event-card.tsx
+++ components/events/event-card.tsx
@@ -7,7 +7,7 @@ import { format } from "date-fns";
 export function EventCard(props: {eventFrontMatter: EventsFrontMatter}) {
     console.log(props.eventFrontMatter);
     
-    return <div className="flex xl:w-1/2 bg-zinc-800 p-4 rounded-md m-8 hover:shadow-gray-700 hover:shadow-md duration-200">
+    return <div className="flex flex-col md:flex-row xl:w-1/2 bg-white shadow-md dark:bg-zinc-800 p-4 rounded-md m-8 hover:shadow-lg duration-200">
         <Image 
             height={250}
             width={250}
@@ -21,15 +21,15 @@ export function EventCard(props: {eventFrontMatter: EventsFrontMatter}) {
             <h2 className="text-3xl capitalize pbs-2 pbe-2 font-semibold">
                 {props.eventFrontMatter?.title}
             </h2>
-            <p className="text-slate-300 font-light plb-2">
-                <span className="text-sm font-thin pie-1">
+            <p className="dark:text-slate-300 font-light plb-2">
+                <span className="text-sm font-light pie-1">
                     On :-
                 </span>
                 {format(new Date(props.eventFrontMatter?.organisedOn as string), 'PPPP')}
                 {}
             </p>
-            <p className="text-slate-300 font-medium">
-                <span className="text-sm font-thin">
+            <p className="dark:text-slate-300 font-medium">
+                <span className="text-sm font-light">
                     Organisers :- <br/>
                 </span>
                 {props.eventFrontMatter?.organisers}
