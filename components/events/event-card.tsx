b98d2d3e2425701f41463d5232d268a5fa53f938
Ganesh Tiwari <ganesht049@gmail.com>
2022-12-16 21:04:41 +0530

    feat: improved event listing page

--- components/events/event-card.tsx
+++ components/events/event-card.tsx
@@ -2,11 +2,12 @@ import React from "react"
 import { EventsFrontMatter } from "../../pages/events"
 import Image from "next/image";
 import config from "../../config";
+import { format } from "date-fns";
 
 export function EventCard(props: {eventFrontMatter: EventsFrontMatter}) {
     console.log(props.eventFrontMatter);
     
-    return <div className="flex">
+    return <div className="flex xl:w-1/2 bg-zinc-800 p-4 rounded-md m-8 hover:shadow-gray-700 hover:shadow-md duration-200">
         <Image 
             height={250}
             width={250}
@@ -15,14 +16,22 @@ export function EventCard(props: {eventFrontMatter: EventsFrontMatter}) {
             objectPosition="relative"
             src={`${config.ASSETS}${props?.eventFrontMatter?.coverImage}`} />
         
-        <div>
-            <h2>
+    
+        <div className="pl-3">
+            <h2 className="text-3xl capitalize pbs-2 pbe-2 font-semibold">
                 {props.eventFrontMatter?.title}
             </h2>
-            <p>
-                {props.eventFrontMatter?.organisedOn}
+            <p className="text-slate-300 font-light plb-2">
+                <span className="text-sm font-thin pie-1">
+                    On :-
+                </span>
+                {format(new Date(props.eventFrontMatter?.organisedOn as string), 'PPPP')}
+                {}
             </p>
-            <p>
+            <p className="text-slate-300 font-medium">
+                <span className="text-sm font-thin">
+                    Organisers :- <br/>
+                </span>
                 {props.eventFrontMatter?.organisers}
             </p>
             <p>
